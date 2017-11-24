18a1f951857c736e7756f2b6455fbe375156cab4
Grant Traynor <grant.traynor@gmail.com>
2017-11-24 11:11:29 +1100

    Update settings.py

--- nadine/settings.py
+++ nadine/settings.py
@@ -21,7 +21,7 @@ SITE_PROTO = "http"
 SITE_ID = 1
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = '/app/static'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ('themes/active/static', 'static', )
